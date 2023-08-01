83062f4a5611b286b0579903b46dd4fd5aaec9ec
Mauricio <fuentesmauricio75@gmail.com>
2023-07-31 21:16:37 -0500

    Finished implementing ifDatabaseIsEmptyShouldFetchNumbersFromApi()

--- app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
+++ app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
@@ -1,7 +1,9 @@
 package com.ics342.labs
 
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
 import io.mockk.verify
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
@@ -36,5 +38,28 @@ internal class NumbersRepositoryTest {
     @Test
     fun ifDatabaseIsEmptyShouldFetchNumbersFromApi() {
         // TODO: implement this test
+
+        // Setup
+        val database = mockk<Database>()
+        val api = mockk<Api>()
+        val number = Number(UUID.randomUUID().toString(), Random.nextInt())
+        val id = number.id
+        var emptyNumberList: List<Number> = emptyList()
+
+        every {database.getAllNumbers()} returns emptyNumberList
+        every {api.getNumbers()} returns listOf(number)
+        every {database.storeNumbers(listOf(number))} just runs
+
+        // Act
+        val repository = NumbersRepository(database, api)
+        val results = repository.fetchNumbers()
+
+        // Assert
+        assertNotNull(results)
+        assertEquals(results, listOf(number))
+
+        verify { database.getAllNumbers() }
+        verify { api.getNumbers()}
+
     }
 }
